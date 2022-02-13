621daaa7ac0f3f3d4c5a43b11316422901cc695f
biancasofia <sofiabrasil44@gmail.com>
2022-02-13 20:03:48 -0300

    Projeto inicial

---
+++ Travelers/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+
+});
+
+
+app.listen(3000, () => console.log("servidor rodando"));
