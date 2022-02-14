95d42e52fe7812776edc074ed24250b1909b83ad
biancasofia <sofiabrasil44@gmail.com>
2022-02-14 09:30:01 -0300

    criação de interface e ligação com aplicação

--- Travelers/index.js
+++ Travelers/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
 
 app.get('', function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
---
+++ Travelers/public/js/itens.js
@@ -0,0 +1,55 @@
+﻿
+
+
+
+function listar() {
+
+	var idCapitalDestino = "select-capital-destino";
+	var idCapitalOrigem = "select-capital-origem";
+    var capital = [
+		"Aracajú",
+		"Belém",
+		"Belo Horizonte",
+		"Boa Vista",
+		"Brasília",
+		"Campo Grande",
+		"Cuiabá",
+		"Curitiba",
+		"Florianópolis",
+		"Fortaleza",
+		"Goiânia",
+		"João Pessoa",
+		"Maceió",
+		"Manaus",
+		"Natal",
+		"Palmas",
+		"Porto Alegre",
+		"Porto Velho",
+		"Recife",
+		"Rio Branco",
+		"Rio de Janeiro",
+		"Salvador",
+		"São Luis",
+		"São Paulo",
+		"Teresina",
+		"Vitória"]
+    
+	gerar(capital, idCapitalOrigem);
+	gerar(capital, idCapitalDestino);
+    
+}
+function gerar(lista, id) {
+	let optionSelect = document.getElementById(id)
+	for (let i = 0; i < lista.length; i++) {
+		let option = document.createElement("option")
+		option.value = lista[i]
+		option.text = lista[i]
+		optionSelect.appendChild(option)
+	}
+
+}
+
+listar()
+
+
+
